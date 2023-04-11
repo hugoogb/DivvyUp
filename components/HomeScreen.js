5d48e3d4d498508d06aebcc8ce01248f92f1883f
Hugo GB <hugogaben8.02@gmail.com>
2023-04-11 14:38:31 +0200

    add HomeScreen svg and update text

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,16 +1,23 @@
-import { SafeAreaView, Button, StyleSheet } from "react-native";
+import { SafeAreaView, View, Button, StyleSheet, Text } from "react-native";
 
 export const HomeScreen = ({ navigation }) => {
 	return (
 		<SafeAreaView style={styles.container}>
-			<Button
-				title="Go to Jane's profile"
-				onPress={() => navigation.navigate("Profile", { name: "Jane" })}
-			/>
-			<Button
-				title='See group'
-				onPress={() => navigation.navigate("Group")}
-			/>
+			<View style={styles.containerTitle}>
+				<Text style={styles.title}>DivvyUp</Text>
+			</View>
+			<View style={styles.container}>
+				<Button
+					title="Go to Jane's profile"
+					onPress={() =>
+						navigation.navigate("Profile", { name: "Jane" })
+					}
+				/>
+				<Button
+					title='See group'
+					onPress={() => navigation.navigate("Group")}
+				/>
+			</View>
 		</SafeAreaView>
 	);
 };
@@ -20,5 +27,14 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: "center",
 		justifyContent: "center",
+	},
+	containerTitle: {
+		flex: 4,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 48,
+		fontWeight: "700",
 	},
 });
