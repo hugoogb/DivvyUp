42aebdc3f85ea5f999e90a05bbbc7ac46073b1ba
Hugo GB <hugogaben8.02@gmail.com>
2023-04-11 18:00:22 +0200

    updated signup and login screens

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,3 +1,48 @@
+import { StyleSheet, SafeAreaView, View, Text } from "react-native";
+import SvgLogin from "../assets/undraw_login.svg";
+
 export const LoginScreen = () => {
-	return <></>;
+	return (
+		<SafeAreaView style={[styles.container, styles.wrapper]}>
+			<View style={styles.containerTitle}>
+				<Text style={styles.title}>Login</Text>
+				<SvgLogin width={300} height={300}></SvgLogin>
+				<Text style={styles.textDescription}>
+					Log into your DivvyUp account
+				</Text>
+			</View>
+			<View style={styles.container}>
+				<Text>Login</Text>
+			</View>
+		</SafeAreaView>
+	);
 };
+
+const styles = StyleSheet.create({
+	wrapper: {
+		backgroundColor: "#EEE3D9",
+	},
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	containerTitle: {
+		flex: 4,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 56,
+		fontWeight: "800",
+		marginBottom: 20,
+		color: "#10181F",
+	},
+	textDescription: {
+		fontSize: 20,
+		marginLeft: 50,
+		marginRight: 50,
+		marginTop: 20,
+		textAlign: "center",
+	},
+});
--- components/SignUpScreen.js
+++ components/SignUpScreen.js
@@ -1,3 +1,48 @@
+import { StyleSheet, SafeAreaView, View, Text } from "react-native";
+import SvgSignUp from "../assets/undraw_signup.svg";
+
 export const SignUpScreen = () => {
-	return <></>;
+	return (
+		<SafeAreaView style={[styles.container, styles.wrapper]}>
+			<View style={styles.containerTitle}>
+				<Text style={styles.title}>SignUp</Text>
+				<SvgSignUp width={300} height={300}></SvgSignUp>
+				<Text style={styles.textDescription}>
+					Create a DivvyUp account if you don't have one already
+				</Text>
+			</View>
+			<View style={styles.container}>
+				<Text>SignUp</Text>
+			</View>
+		</SafeAreaView>
+	);
 };
+
+const styles = StyleSheet.create({
+	wrapper: {
+		backgroundColor: "#EEE3D9",
+	},
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	containerTitle: {
+		flex: 4,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 56,
+		fontWeight: "800",
+		marginBottom: 20,
+		color: "#10181F",
+	},
+	textDescription: {
+		fontSize: 20,
+		marginLeft: 50,
+		marginRight: 50,
+		marginTop: 20,
+		textAlign: "center",
+	},
+});
