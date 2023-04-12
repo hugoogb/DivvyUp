3810e12013d80f623e99832354bb997ce3307169
Hugo GB <hugogaben8.02@gmail.com>
2023-04-12 11:06:46 +0200

    add firebase auth

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 // Optionally import the services that you want to use
-// import {...} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 // import {...} from "firebase/database";
 // import {...} from "firebase/firestore";
 // import {...} from "firebase/functions";
@@ -21,3 +21,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
