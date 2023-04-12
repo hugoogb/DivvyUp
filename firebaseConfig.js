9ff9b9901d443734d9de498a78e802f00fad7899
Hugo GB <hugogaben8.02@gmail.com>
2023-04-12 10:56:51 +0200

    add firebase and firebaseConfig

---
+++ firebaseConfig.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+	apiKey: "AIzaSyBUpj-AYMpAidNv17qmcexpQFYpWq_AIt8",
+	authDomain: "divvyup-74851.firebaseapp.com",
+	projectId: "divvyup-74851",
+	storageBucket: "divvyup-74851.appspot.com",
+	messagingSenderId: "655704363479",
+	appId: "1:655704363479:web:7fbb4df0e9c93060bbfb92",
+	measurementId: "G-SWM1T81NQ8",
+};
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
