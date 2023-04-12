9af9b6a48560909155085c0f10b507fc0a49ae17
Hugo GB <hugogaben8.02@gmail.com>
2023-04-12 12:28:02 +0200

    add onAuthStateChanged

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import {
 	createUserWithEmailAndPassword,
 	signInWithEmailAndPassword,
 	signOut,
+	onAuthStateChanged,
 } from "firebase/auth";
 import { appFirebase } from "./firebaseConfig";
 
@@ -133,6 +134,19 @@ export default function App() {
 		}),
 		[]
 	);
+
+	// TODO : set profile image, username ...
+	onAuthStateChanged(auth, (user) => {
+		if (user) {
+			// User is signed in, see docs for a list of available properties
+			// https://firebase.google.com/docs/reference/js/firebase.User
+			const uid = user.uid;
+			// ...
+		} else {
+			// User is signed out
+			// ...
+		}
+	});
 
 	return (
 		<AuthContext.Provider value={authContext}>
