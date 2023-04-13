5cbb9123df1c553f3ad3fd716a1f92e8a704c88f
Hugo GB <hugogaben8.02@gmail.com>
2023-04-13 22:59:00 +0200

    get info of a group using groupId

--- components/GroupCard.js
+++ components/GroupCard.js
@@ -1,10 +1,47 @@
 import { View, Text } from "react-native";
 import { globalStyles } from "../styles/global.styles";
+import { useEffect, useState } from "react";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
 
 export const GroupCard = ({ groupId }) => {
+	const [group, setGroup] = useState({});
+
+	useEffect(() => {
+		const getGroupDataAsync = async () => {
+			const docSnap = await getDoc(doc(db, "groups", groupId));
+
+			if (docSnap.exists()) {
+				setGroup(docSnap.data());
+			} else {
+				// docSnap.data() will be undefined in this case
+				setGroup({});
+			}
+		};
+
+		getGroupDataAsync();
+	}, []);
+
 	return (
-		<View style={globalStyles.container}>
-			<Text style={globalStyles.textDescription}>{groupId}</Text>
+		<View
+			style={[
+				globalStyles.container,
+				{ borderWidth: 2 },
+				{ borderColor: "#10181F" },
+				{ borderRadius: 10 },
+				{ marginBottom: 20 },
+			]}
+		>
+			<Text style={globalStyles.textDescription}>{group.name}</Text>
+			<Text
+				style={[
+					{ padding: 5 },
+					{ marginBottom: 5 },
+					{ marginHorizontal: 5 },
+				]}
+			>
+				{group.members}
+			</Text>
 		</View>
 	);
 };
