b51eee2e67ec774bd721205a00c0c34827c198d4
Hugo GB <hugogaben8.02@gmail.com>
2023-05-08 21:30:30 +0200

    update userContext onSnapshot

--- App.js
+++ App.js
@@ -11,10 +11,12 @@ import {
 	signOut,
 } from "firebase/auth";
 import { auth, db } from "./firebaseConfig";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc, onSnapshot } from "firebase/firestore";
 import { useMemo, useReducer, useState } from "react";
 import { AuthContext } from "./context/auth/AuthContext";
 import { UserContext } from "./context/user/UserContext";
+import { HelpModal } from "./components/modal/HelpModal";
+import { InviteModal } from "./components/modal/InviteModal";
 
 const Stack = createStackNavigator();
 
@@ -29,6 +31,7 @@ export default function App() {
 						...prevState,
 						isSignout: false,
 						userToken: action.token,
+						unsubscribe: action.unsubscribe,
 					};
 				case "SIGN_OUT":
 					return {
@@ -41,11 +44,47 @@ export default function App() {
 		{
 			isSignout: false,
 			userToken: null,
+			unsubscribe: null,
 		}
 	);
 
 	const authContext = useMemo(
 		() => ({
+			signUp: (email, password, name) => {
+				createUserWithEmailAndPassword(auth, email, password)
+					.then(async (userCredential) => {
+						const user = {
+							uid: userCredential.user.uid,
+							email: userCredential.user.email,
+							name: name,
+						};
+
+						const docRef = doc(
+							db,
+							"users",
+							userCredential.user.uid
+						);
+
+						await setDoc(docRef, user);
+
+						setUserContext(user);
+
+						const unsubscribe = onSnapshot(docRef, (snapshot) => {
+							setUserContext(snapshot.data());
+						});
+
+						dispatch({
+							type: "SIGN_IN",
+							token: userCredential.user.getIdToken(),
+							unsubscribe: unsubscribe, // Save the unsubscribe function in state
+						});
+					})
+					.catch((error) => {
+						// const errorCode = error.code;
+						const errorMessage = error.message;
+						alert(errorMessage);
+					});
+			},
 			signIn: (email, password) => {
 				signInWithEmailAndPassword(auth, email, password)
 					.then(async (userCredential) => {
@@ -59,40 +98,20 @@ export default function App() {
 
 						setUserContext(docSnap.data());
 
-						dispatch({
-							type: "SIGN_IN",
-							token: userCredential.user.getIdToken(),
-						});
-					})
-					.catch((error) => {
-						// const errorCode = error.code;
-						const errorMessage = error.message;
-						alert(errorMessage);
-					});
-			},
-			signOut: () => {
-				signOut(auth);
-				dispatch({ type: "SIGN_OUT" });
-			},
-			signUp: (email, password, name) => {
-				createUserWithEmailAndPassword(auth, email, password)
-					.then(async (userCredential) => {
-						const user = {
-							uid: userCredential.user.uid,
-							email: userCredential.user.email,
-							name: name,
-						};
-
-						setUserContext(user);
-
-						await setDoc(
-							doc(db, "users", userCredential.user.uid),
-							user
+						const unsubscribe = onSnapshot(
+							docRef,
+							(snapshot) => {
+								setUserContext(snapshot.data());
+							},
+							(error) => {
+								console.log(`error: ${error}`); // TODO: fix error
+							}
 						);
 
 						dispatch({
 							type: "SIGN_IN",
 							token: userCredential.user.getIdToken(),
+							unsubscribe: unsubscribe, // Save the unsubscribe function in state
 						});
 					})
 					.catch((error) => {
@@ -100,6 +119,28 @@ export default function App() {
 						const errorMessage = error.message;
 						alert(errorMessage);
 					});
+			},
+			signOut: async () => {
+				// Retrieve the unsubscribe function from state
+				const { unsubscribe } = state;
+
+				// Stop listening for updates to the user's document
+				if (unsubscribe) {
+					await unsubscribe(); // Unsubscribe from the snapshot listener
+					console.log("Unsubscribed from onSnapshot");
+				}
+
+				try {
+					// Sign out and wait for it to complete
+					await signOut(auth);
+					console.log("User signed out successfully");
+
+					// Clear the user context and dispatch the SIGN_OUT action
+					setUserContext({});
+					dispatch({ type: "SIGN_OUT" });
+				} catch (error) {
+					console.log("Error signing out:", error);
+				}
 			},
 		}),
 		[]
@@ -121,7 +162,7 @@ export default function App() {
 							/>
 						) : state.userToken === null ? (
 							// No token found, user isn't signed in
-							<>
+							<Stack.Group>
 								<Stack.Screen
 									name='GetStarted'
 									component={GetStartedScreen}
@@ -158,17 +199,27 @@ export default function App() {
 											: "push",
 									}}
 								/>
-							</>
+							</Stack.Group>
 						) : (
 							// User is signed in
-							<Stack.Screen
-								name='Home'
-								component={HomeNavigator}
-								options={{
-									headerShown: false,
-								}}
-							/>
+							<Stack.Group>
+								<Stack.Screen
+									name='Home'
+									component={HomeNavigator}
+									options={{
+										headerShown: false,
+									}}
+								/>
+							</Stack.Group>
 						)}
+						{/* Common modal screens */}
+						<Stack.Group screenOptions={{ presentation: "modal" }}>
+							<Stack.Screen name='Help' component={HelpModal} />
+							<Stack.Screen
+								name='Invite'
+								component={InviteModal}
+							/>
+						</Stack.Group>
 					</Stack.Navigator>
 				</NavigationContainer>
 			</UserContext.Provider>
